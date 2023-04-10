79b1f5e543f1219ced151fbcddf604312a810620
19495 <19495@ALRAWI.myfiosgateway.com>
2023-04-10 12:21:31 -0400

    First commit

---
+++ src/Demo5/Test5.java
@@ -0,0 +1,11 @@
+package Demo5;
+
+public class Test5 {
+	public static void main(String[] args) {
+		System.out.println("Test5 In process...");
+	}
+	public void getResult(int a, int b) {
+		System.out.println(a + b);
+	}
+
+}
