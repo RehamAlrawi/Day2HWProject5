ad527b2b525a336f5b7d1f1bb514ff0373c189ac
19495 <19495@ALRAWI.myfiosgateway.com>
2023-04-10 12:23:40 -0400

    divid method

--- src/Demo5/Test5.java
+++ src/Demo5/Test5.java
@@ -4,8 +4,9 @@ public class Test5 {
 	public static void main(String[] args) {
 		System.out.println("Test5 In process...");
 	}
-	public void getResult(int a, int b) {
-		System.out.println(a + b);
+	public void divid(int a, int b) {
+		System.out.println(a / b);
 	}
+	
 
 }
